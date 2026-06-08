18150ff083be88acb3cba57016da516e730d0b7c
Muammad Shaheer <shaheerkhan0012345@gmail.com>
2026-06-08 12:54:30 -0700

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
+
+
+//NO1 
 function myFunction(){
     console.log("Shaheer Is Great")
 }
 
 
 myFunction();
+
+
+// NO 2
+
+
+function shaheer(msg, n){
+    console.log(msg, n)
+}
+
+
+shaheer("Love U Jani",100)
+
+
+
+//No 3 (sum calculation)
+
+function sum(x , y){
+
+    console.log(x+y);
+
+}
+
+
+sum();
+
+
+//No4
+
+function func(x,y){
+
+    s = x+y;
+
+    return s;
+}
+
+
+let val = func(3 ,4)
+
+console.log(val);
