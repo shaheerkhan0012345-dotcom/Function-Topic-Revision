789ab4188dfa3fd57e74d996864639e7b9388a92
Muammad Shaheer <shaheerkhan0012345@gmail.com>
2026-06-09 06:03:34 -0700

    third commit

--- script.js
+++ script.js
@@ -1,48 +1,102 @@
 
 
 //NO1 
-function myFunction(){
-    console.log("Shaheer Is Great")
-}
+// function myFunction(){
+//     console.log("Shaheer Is Great")
+// }
 
 
-myFunction();
+// myFunction();
 
 
 // NO 2
 
 
-function shaheer(msg, n){
-    console.log(msg, n)
-}
+// function shaheer(msg, n){
+//     console.log(msg, n)
+// }
 
 
-shaheer("Love U Jani",100)
+// shaheer("Love U Jani",100)
 
 
 
 //No 3 (sum calculation)
 
-function sum(x , y){
+// function sum(x , y){
 
-    console.log(x+y);
+//     console.log(x+y);
 
-}
+// }
 
 
-sum();
+// sum();
 
 
 //No4
 
-function func(x,y){
+// function func(x,y){
 
-    s = x+y;
+//     s = x+y;
 
-    return s;
+//     return s;
+// }
+// let val = func(3 ,4)
+
+// console.log(val);
+
+
+
+// No 5 (ARROW FUNCTION) COMPACT WAY
+ 
+
+let sumfunc = (a,b) =>{
+     console.log(a + b)
 }
 
 
-let val = func(3 ,4)
 
-console.log(val);
+let mulfunc = (a , b) =>{
+    console.log(a * b)
+}
+
+
+
+//QUESTION
+
+// function task(str){
+
+//     let count = 0;
+//     for( let char of str){
+//         if(
+//             char=== "a" || 
+//             char === "e" || 
+//             char === "i" || 
+//             char === "o" || 
+//             char === "u"){
+
+//                 count++;
+//         }
+//     }
+
+//     console.log(count)
+// }
+
+
+
+let vowelCount = (str) =>{
+        let count = 0;
+    for( let char of str){
+        if(
+            char=== "a" || 
+            char === "e" || 
+            char === "i" || 
+            char === "o" || 
+            char === "u"){
+
+                count++;
+        }
+    }
+
+    console.log(count)
+}
